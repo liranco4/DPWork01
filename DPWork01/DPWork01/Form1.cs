3fa4f89bf9fadbc4f52454db428f7c1349b2e50c
LENOVO-PC\Opal <opapter@gmail.com>
2017-07-18 14:06:23 +0300

    add to form new name

--- DPWork01/DPWork01/Form1.cs
+++ DPWork01/DPWork01/Form1.cs
@@ -15,5 +15,10 @@ namespace DPWork01
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = "first";
+        }
     }
 }
